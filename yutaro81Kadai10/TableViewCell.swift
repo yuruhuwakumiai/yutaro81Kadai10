c5b53ea196eec0be1928c4aadeff16bdba0c5d80
yutaro <89335899+yuruhuwakumiai@users.noreply.github.com>
2021-11-28 16:50:05 +0900

    修正

--- yutaro81Kadai10/TableViewCell.swift
+++ yutaro81Kadai10/TableViewCell.swift
@@ -9,15 +9,19 @@ import UIKit
 
 class TableViewCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    @IBOutlet weak private var label1: UILabel!
+    @IBOutlet weak private var label2: UILabel!
+
+    func configure(name: String, index: Int) {
+        // ラベルのtextを表示する
+        label1.text = name
+        label2.text = "\(index + 1)番目の都道府県です"
+        //cellの色を変える　contentView.backgroundColor
+        let colors: [UIColor] = [
+            UIColor(red: 1.0, green: 0.5, blue: 0.5, alpha: 1.0),
+            UIColor(red: 0.5, green: 1.0, blue: 0.5, alpha: 1.0),
+            UIColor(red: 0.5, green: 0.5, blue: 1.0, alpha: 1.0),
+]
+        contentView.backgroundColor = colors[index % colors.count]
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
 }
--- yutaro81Kadai10/ViewController.swift
+++ yutaro81Kadai10/ViewController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
-    private var count = 0
     // 配列の行数、コンマの位置
     private let placeData = ["北海道", "青森県", "岩手県", "宮城県", "秋田県",
                              "山形県", "福島県", "茨城県", "栃木県", "群馬県",
@@ -23,25 +22,34 @@ class ViewController: UIViewController, UITableViewDataSource {
 
     // tableviewはセクションとrowで構成されている　セクションの中にrowという行数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return placeData.count
+        //        return いらない　フィードバック１
+        placeData.count
     }
     // 行数が20と決まったとき20回呼ばれる　表示する内容を返す
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // identifier という空の変数を作成して
-        var identifier = ""
-        if indexPath.row % 3 == 1 {
-            identifier = "Cell1"
-        }
-        if indexPath.row % 3 == 2 {
-            identifier = "Cell2"
-        }
-        if indexPath.row % 3 == 0 {
-            identifier = "Cell3"
-        }
-let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)// 変数identifierを設定
-if let tableLabel1 = cell.contentView.viewWithTag(1)as? UILabel { tableLabel1.text = placeData[indexPath.row] }
-        // indexPath.rowでその行数の番号が取得できる
-if let tableLabel2 = cell.contentView.viewWithTag(2)as? UILabel { tableLabel2.text = "\(indexPath.row + 1)番目の都道府県です" }
+        // フィードバック後訂正 swiftlintのルールを１行だけ変更する
+        // swiftlint:disable:next force_cast
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PlaceCell",for: indexPath) as! TableViewCell
+        cell.configure(name: placeData[indexPath.row], index: indexPath.row)
+
         return cell
     }
 }
+//        // identifier という空の変数を作成して
+//        var identifier = ""
+//        if indexPath.row % 3 == 1 {
+//            identifier = "Cell1"
+//        }
+//        if indexPath.row % 3 == 2 {
+//            identifier = "Cell2"
+//        }
+//        if indexPath.row % 3 == 0 {
+//            identifier = "Cell3"
+//        }
+// let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)// 変数identifierを設定
+// if let tableLabel1 = cell.contentView.viewWithTag(1)as? UILabel { tableLabel1.text = placeData[indexPath.row] }
+//        // indexPath.rowでその行数の番号が取得できる
+// if let tableLabel2 = cell.contentView.viewWithTag(2)as? UILabel { tableLabel2.text = "\(indexPath.row + 1)番目の都道府県です" }
+//        return cell
+
+//mark
